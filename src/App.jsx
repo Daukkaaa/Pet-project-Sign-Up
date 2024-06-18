c5aabaedd57634673ba5be89060769a125e0a0c0
Daulet <zumgalidaulet@gmail.com>
2024-06-18 21:35:08 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Components/Form.jsx";
 import './main.css';
 
 const App = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    country: '',
+    password: ''
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div>
-      <Form />
+      <Form formData={formData} handleChange={handleChange} />
     </div>
   )
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,68 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoEye, IoEyeOff } from "react-icons/io5";
 
-const Form = () => {
+const Form = (props) => {
+    const [visiblePassword, setVisiblePassword] = useState(false);
+
     return (
         <div className="form-container">
             <form className="signup-form">
                 <h2>Registration Form</h2>
+                <hr />
                 <div className='full-name'>
                     <div>
-                        <label htmlFor="name">First Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            required
-                        />
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" id="firstName" name="firstName" value={props.formData.firstName} onChange={props.handleChange} required />
                     </div>
                     <div>
-                        <label htmlFor="lastname">Last Name</label>
-                        <input
-                            type="text"
-                            id="lastname"
-                            name="lastname"
-                            required
-                        />
+                        <label htmlFor="lastName" id="lastname">Last Name</label>
+                        <input type="text" id="lastName" name="lastName" value={props.formData.lastName} onChange={props.handleChange} />
                     </div>
                 </div>
                 <div className='place'>
                     <div>
                         <label htmlFor="country">Country</label>
-                        <input
-                            type="text"
-                            id="country"
-                            name="country"
-                            required
-                        />
+                        <input type="text" id="country" name="country" value={props.formData.country} onChange={props.handleChange} required />
                     </div>
                     <div>
                         <label htmlFor="phone">Phone</label>
-                        <input
-                            type="tel"
-                            id="phone"
-                            name="phone"
-                            required
-                        />
+                        <input type="tel" id="phone" name="phone" value={props.formData.phone} onChange={props.handleChange} required />
                     </div>
                 </div>
                 <div className='bio'>
                     <div>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            required
-                        />
+                        <label htmlFor="email">E-mail</label>
+                        <input type="email" id="email" name="email" value={props.formData.email} onChange={props.handleChange} required />
                     </div>
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            required
-                        />
+                        <div className='input-icon'>
+                            <input type={visiblePassword ? "text" : "password"} className='password-input' id="password" name="password" value={props.formData.password} onChange={props.handleChange} required />
+                            <span className='eye' onClick={() => setVisiblePassword(!visiblePassword)}>
+                                {visiblePassword ? <IoEye /> : <IoEyeOff />}
+                            </span>
+                        </div>
                     </div>
                 </div>
                 <input type="submit" value="Sign Up" />
